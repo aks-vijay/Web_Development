4d6fcdc79d8d63695e4f2a814d80f7d8fd112399
Akshay V <83549571+aks-vijay@users.noreply.github.com>
2022-05-28 12:32:28 -0400

    Create Love calculator.js

---
+++ JavaScript - Micro Projects/Love calculator.js
@@ -0,0 +1,27 @@
+// prompt the user
+alert("Welcome to Love Calculator");
+var yourName = prompt("What is your name?");
+var yourLoversName = prompt("What is your lover's name?");
+
+// calculate the score
+function calcScore() {
+    var n = Math.random();
+    score = n*100;
+    score = Math.floor(score)+1;
+    return score;
+}
+
+lovescore = calcScore();
+
+// return the score to the user
+if (lovescore > 70) {
+    alert("You both have a love score of " + 
+          lovescore + "%");
+}
+if (lovescore > 40 && lovescore <= 70) {
+    alert("You both have a love score of " + 
+          lovescore + "%";
+}
+if (lovescore <=30 ) {
+    alert("I'm sorry, you both have a very love score of only " + lovescore + "%");
+}
