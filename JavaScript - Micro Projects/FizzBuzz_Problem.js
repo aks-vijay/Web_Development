d284feff741063b6b1160a0470010d98264080ba
Akshay V <83549571+aks-vijay@users.noreply.github.com>
2022-05-29 08:33:07 -0400

    Update FizzBuzz_Problem.js
    added loop

--- JavaScript - Micro Projects/FizzBuzz_Problem.js
+++ JavaScript - Micro Projects/FizzBuzz_Problem.js
@@ -3,19 +3,22 @@ var count = 1;
 
 function fizzBuzz() {
 
-    if (count % 3 === 0) {
-        output.push("Fizz");
+    while (count < 100) {
+
+        if (count % 3 === 0) {
+            output.push("Fizz");
+        }
+        else if (count % 5 === 0) {
+            output.push("Buzz");
+        }
+        else if (count % 3 === 0 && count % 5 == 0) {
+            output.push("FizzBuzz");
+        }
+        else {
+            output.push(count);
+        }
+        count++;  
     }
-    else if (count % 5 === 0) {
-        output.push("Buzz");
-    }
-    else if (count % 3 === 0 && count % 5 == 0) {
-        output.push("FizzBuzz");
-    }
-    else {
-        output.push(count);
-    }
-    count++;
     console.log(output);
 }
 
